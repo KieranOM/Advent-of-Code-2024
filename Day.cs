fd2ab3b8f04b2c4f46ff478ab9020a9ab4344697
Kieran <Kieran@CBM-Logix.com>
2024-12-03 21:48:54 +0000

    Setup solution

---
+++ Day.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace AoC;
+
+public abstract class Day
+{
+    public abstract int Number { get; }
+    private string LogPrefix => $"[Day {Number}] ";
+
+    protected abstract void Run(in string[] input);
+
+    public void Run()
+    {
+        string inputPath = $"Day{Number}/input.txt";
+        if (!File.Exists(inputPath))
+        {
+            Log("Input file does not exist");
+            return;
+        }
+
+        if (!TryReadAllLines(inputPath, out string[] input))
+        {
+            Log("Unable to read input file");
+            return;
+        }
+
+        Log("Running...");
+
+        var stopwatch = Stopwatch.StartNew();
+        Run(input);
+        stopwatch.Stop();
+
+        Log($"Time elapsed: {stopwatch.Elapsed}");
+    }
+
+    private static bool TryReadAllLines(in string path, out string[] lines)
+    {
+        try
+        {
+            lines = File.ReadAllLines(path);
+            return true;
+        }
+        catch
+        {
+            lines = [];
+            return false;
+        }
+    }
+
+    protected void Log(in string message)
+    {
+        Console.WriteLine(LogPrefix + message);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace AoC;
+
+internal static class Program
+{
+    private static readonly Day[] Days =
+    [
+        new Day1()
+    ];
+
+    public static void Main(string[] args)
+    {
+        string input = GetDayNumberInput(args);
+        if (!ValidateDayNumberInput(input, out int number))
+        {
+            Console.WriteLine("Invalid day entered (1-25)");
+            return;
+        }
+
+        var day = Days.SingleOrDefault(day => day.Number == number);
+        if (day == null)
+        {
+            Console.WriteLine($"Day {number} does not exist");
+            return;
+        }
+
+        day.Run();
+    }
+
+    private static string GetDayNumberInput(in string[] args)
+    {
+        if (args is { Length: > 0 }) return args[0];
+        Console.Write("Day: ");
+        return Console.ReadLine() ?? "";
+    }
+
+    private static bool ValidateDayNumberInput(string input, out int number)
+    {
+        return int.TryParse(input, out number) && number is >= 1 and <= 25;
+    }
+}
